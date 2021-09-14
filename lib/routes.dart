e91bd05cc791c32b13b201076eb13daa052b81b5
FerMod <FerMod@users.noreply.github.com>
2021-09-14 18:48:21 +0000

    deploy: 4332198981b14980ac276ea45e5d8cbb877d7bc9

---
+++ lib/routes.dart
@@ -0,0 +1,155 @@
+import 'package:flutter/material.dart';
+
+import 'screens/notes_list.dart';
+import 'screens/settings.dart';
+import 'screens/sign_in.dart';
+import 'screens/sign_up.dart';
+import 'src/utils/device_type.dart';
+
+class AppRoute {
+  static const String notes = '/notes';
+  static const String settings = '/settings';
+  static const String signIn = '/signIn';
+  static const String signUp = '/signUp';
+
+  static Map<String, WidgetBuilder> routes = <String, WidgetBuilder>{
+    notes: (context) => NotesListScreen(),
+    settings: (context) => const SettingsScreen(),
+    signIn: (context) => const SignInScreen(),
+    signUp: (context) => const SignUpScreen(),
+  };
+}
+
+typedef PathWidgetBuilder = Widget Function(BuildContext, String?);
+
+class Path {
+  const Path(this.pattern, this.builder);
+
+  /// A RegEx string for route matching.
+  final String pattern;
+
+  /// The builder for the associated pattern route. The first argument is the
+  /// [BuildContext] and the second argument a RegEx match if that is included
+  /// in the pattern.
+  ///
+  /// ```dart
+  /// Path(
+  ///   'r'^/demo/([\w-]+)$',
+  ///   (context, matches) => Page(argument: match),
+  /// )
+  /// ```
+  final PathWidgetBuilder builder;
+}
+
+class RouteConfiguration {
+  /// List of [Path] to for route matching. When a named route is pushed with
+  /// [Navigator.pushNamed], the route name is matched with the [Path.pattern]
+  /// in the list below. As soon as there is a match, the associated builder
+  /// will be returned. This means that the paths higher up in the list will
+  /// take priority.
+  static List<Path> paths = [
+    Path(
+      r'^' + AppRoute.notes,
+      (context, match) => NotesListScreen(),
+    ),
+    Path(
+      r'^' + AppRoute.settings,
+      (context, match) => const SettingsScreen(),
+    ),
+    Path(
+      r'^' + AppRoute.signIn,
+      (context, match) => const SignInScreen(),
+    ),
+    Path(
+      r'^' + AppRoute.signUp,
+      (context, match) => const SignUpScreen(),
+    ),
+  ];
+
+  /// The route generator callback used when the app is navigated to a named
+  /// route. Set it on the [MaterialApp.onGenerateRoute] or
+  /// [WidgetsApp.onGenerateRoute] to make use of the [paths] for route
+  /// matching.
+  static Route<dynamic>? onGenerateRoute(RouteSettings settings) {
+    for (final path in paths) {
+      final regExpPattern = RegExp(path.pattern, caseSensitive: false);
+      if (settings.name != null && regExpPattern.hasMatch(settings.name!)) {
+        final firstMatch = regExpPattern.firstMatch(settings.name!);
+        final match = firstMatch?.groupCount == 1 ? firstMatch?.group(1) : null;
+
+        if (regExpPattern.hasMatch(AppRoute.settings)) {
+          return SettingsRouteBuilder(
+            builder: (context) => path.builder(context, match),
+            settings: settings,
+          );
+        }
+
+        if (DeviceType.isDesktopOrWeb) {
+          return NoAnimationMaterialPageRoute<void>(
+            builder: (context) => path.builder(context, match),
+            settings: settings,
+          );
+        }
+        return MaterialPageRoute<void>(
+          builder: (context) => path.builder(context, match),
+          settings: settings,
+        );
+      }
+    }
+
+    // If no match was found, we let [WidgetsApp.onUnknownRoute] handle it.
+    return null;
+  }
+}
+
+class SettingsRouteBuilder<T> extends PageRouteBuilder<T> {
+  SettingsRouteBuilder({
+    required this.builder,
+    RouteSettings? settings,
+    bool maintainState = true,
+    bool fullscreenDialog = false,
+  }) : super(
+          pageBuilder: (context, animation, secondaryAnimation) {
+            return builder(context);
+          },
+          settings: settings,
+          maintainState: maintainState,
+          fullscreenDialog: fullscreenDialog,
+        ) {
+    assert(opaque);
+  }
+
+  final WidgetBuilder builder;
+
+  @override
+  Widget buildTransitions(BuildContext context, Animation<double> animation, Animation<double> secondaryAnimation, Widget child) {
+    var begin = const Offset(1.0, 0.0);
+    var end = Offset.zero;
+    var curve = Curves.easeIn;
+
+    var tween = Tween(begin: begin, end: end);
+    tween.chain(CurveTween(curve: curve));
+
+    return SlideTransition(
+      position: animation.drive(tween),
+      child: child,
+    );
+  }
+}
+
+class NoAnimationMaterialPageRoute<T> extends MaterialPageRoute<T> {
+  NoAnimationMaterialPageRoute({
+    required WidgetBuilder builder,
+    RouteSettings? settings,
+  }) : super(builder: builder, settings: settings);
+
+  @override
+  Widget buildTransitions(
+    BuildContext context,
+    Animation<double> animation,
+    Animation<double> secondaryAnimation,
+    Widget child,
+  ) {
+    return child;
+  }
+}
